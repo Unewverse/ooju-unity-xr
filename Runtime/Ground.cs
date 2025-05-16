3a956d3b3f491dfb2a13a29486e75f5fc07ad36f
jade <jade.park@ooju.world>
2025-05-16 12:58:07 +0200

    fix: Add XR package references to Editor asmdef to resolve XR namespace errors
    - Add Unity.XR.CoreUtils and Unity.XR.Interaction.Toolkit references to OOJUXR.XRSetup.Editor.asmdef
    - Fixes missing XR namespace errors in editor scripts

--- Runtime/Ground.cs
+++ Runtime/Ground.cs
@@ -1,33 +1,16 @@
 using UnityEngine;
-using OojuInteractionPlugin;
 
 namespace OojuXRPlugin
 {
     [RequireComponent(typeof(Collider))]
     public class Ground : MonoBehaviour
     {
-        private void OnCollisionEnter(Collision collision)
-        {
-            // Check if the colliding object has an ObjectAutoAnimator
-            var animator = collision.gameObject.GetComponent<ObjectAutoAnimator>();
-            if (animator != null)
-            {
-                // (Removed) Stop the animation when the object hits the ground
-                // animator.StopAnimation();
-            }
-        }
-
         private void OnCollisionStay(Collision collision)
         {
             // Prevent objects from going through the ground
-            var animator = collision.gameObject.GetComponent<ObjectAutoAnimator>();
-            if (animator != null)
-            {
-                // Keep the object above the ground
-                Vector3 position = collision.transform.position;
-                position.y = Mathf.Max(position.y, transform.position.y + collision.collider.bounds.extents.y);
-                collision.transform.position = position;
-            }
+            Vector3 position = collision.transform.position;
+            position.y = Mathf.Max(position.y, transform.position.y + collision.collider.bounds.extents.y);
+            collision.transform.position = position;
         }
     }
 } 
